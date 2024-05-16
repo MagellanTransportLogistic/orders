83368cefbe07339ceef31032148b8045a0d55926
MyEternity <spddarkforce47@gmail.com>
2024-05-16 09:09:23 +0300

    1. fix city code len 2. added mssql support db engine

--- main/models.py
+++ main/models.py
@@ -11,7 +11,7 @@ from users.models import UserProfile
 
 class City(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name='Идентификатор')
-    code = models.CharField(max_length=12, verbose_name='Код')
+    code = models.CharField(max_length=36, verbose_name='Код')
     name = models.CharField(max_length=256, blank=False, null=False, default='Москва',
                             verbose_name='Наименование')
     full_name = models.CharField(max_length=512, unique=True, blank=False, null=False, default='Нет',
